d3d930f9b0d088f40a00240456092e0e80d22a51
roberto viana <jrviana2017@gmail.com>
2023-01-20 22:23:49 -0300

    Initial Commit

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+     <div class="q-pa-md">
+    <q-btn-toggle
+      v-model="model"
+      toggle-color="primary"
+      :options="[
+        {label: 'One', value: 'one'},
+        {label: 'Two', value: 'two'},
+        {label: 'Three', value: 'three'}
+      ]"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
