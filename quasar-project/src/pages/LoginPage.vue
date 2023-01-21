435e3eb0ce36c70cf2ba7271c69f3a34bd18487f
roberto viana <jrviana2017@gmail.com>
2023-01-20 23:53:55 -0300

    adicionar card

--- quasar-project/src/pages/LoginPage.vue
+++ quasar-project/src/pages/LoginPage.vue
@@ -1,6 +1,25 @@
 <template>
   <q-page class="flex flex-center">
-    <p class="text-h2">Login</p>
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" ratio="1" width="200px" />
+
+      <q-card-section>
+        <div class="text-h5">Login</div>
+        
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Title
+        </div>
+      </q-img>
+    </q-card>
   </q-page>
 </template>
 
